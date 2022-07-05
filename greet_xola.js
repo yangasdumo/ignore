cccf3afff5eefa41557219641362b91659a10313
Yanga <yangasiqiki@gmail.com>
2022-07-05 14:24:21 +0200

    used figlet

--- greet_xola.js
+++ greet_xola.js
@@ -2,9 +2,21 @@
 
 // console.log(greet('Xola'))
 
-import chalk from 'chalk';
-//import the greet module that is in the current folder
-import greet from './greet.js';
+// import chalk from 'chalk';
+// //import the greet module that is in the current folder
+// import greet from './greet.js';
 
-const styledMessage = chalk.bgBlue.black(greet('Xola'));
-console.log(styledMessage)
+// const styledMessage = chalk.bgBlue.black(greet('Xola'));
+// console.log(styledMessage)
+
+
+import figlet from 'figlet';
+
+figlet('greet_xola.js', function(err, data) {
+    if (err) {
+        console.log('Something went wrong...');
+        console.dir(err);
+        return;
+    }
+    console.log(data)
+});
